9d11126e5c0e94dc09a1f9b059f6b33b5d6b69e5
Joel A. Jaffe <joel@jaffesd.com>
2026-02-05 22:25:29 -0800

    Basic logic in `Portaklon.hpp`

--- Portaklon.hpp
+++ Portaklon.hpp
@@ -8,65 +8,90 @@ Treble: Ranges [0-1], 0.5 default
 Level: Ranges [0-1], 0.5 default
 */
 
+#include <memory>
+#include <vector>
+#include "src/CommonProcessors/InputBuffer.h"
+#include "src/CommonProcessors/ToneControl.h"
+#include "src/CommonProcessors/OutputBuffer.h"
+#include "src/GainStageProcessors/PreAmpStage.h"
+#include "src/GainStageProcessors/AmpStage.h"
+#include "src/GainStageProcessors/ClippingStage.h"
+#include "src/GainStageProcessors/FF1Current.h"
+#include "src/GainStageProcessors/FeedForward2.h"
+#include "src/GainStageProcessors/SummingAmp.h"
+
 class Portaklon {
 private:
   InputBuffer mInputBuffer;
-  PreAmpStage mPreAmpStage;
+  std::unique_ptr<PreAmpStage> mPreAmpStage;
   AmpStage mAmpStage;
-  ClippingStage mClippingStage;
-  FF1Current mFF1;
-  FeedForward2 mFF2;
+  std::unique_ptr<ClippingStage> mClippingStage;
+  std::unique_ptr<FF1Current> mFF1;
+  std::unique_ptr<FeedForward2> mFF2;
   SummingAmp mSummingAmp;
   ToneControl mToneControl;
   OutputBuffer mOutputBuffer;
 
+  std::vector<float> tempBuffers[10]; // temporary buffers for processing
+
 public:
 
-  void init(double sampleRate, int blockSize) {}
+  void init(float sampleRate, int blockSize) {
+    mPreAmpStage = std::make_unique<PreAmpStage>(sampleRate, blockSize);
+    mClippingStage = std::make_unique<ClippingStage>(sampleRate);
+    mFF2 = std::make_unique<FeedForward2>(sampleRate);
+    mFF1 = std::make_unique<FF1Current>(*mPreAmpStage, sampleRate);
+
+    for (auto& buf : tempBuffers) {
+      buf.resize(blockSize);
+    }
+
+    // Set defaults
+    setGain(0.5f);
+    setTreble(0.5f);
+    setLevel(0.5f);
+  }
 
   void process(float* inputBuffer, float* outputBuffer, const int numSamples) {
-    
-    // do all operations on outputBuffer
-    std::memcpy(outputBuffer, inputBuffer, numSamples * sizeof(float));
+    // Input -> InputBuffer -> PreAmpStage
+    mInputBuffer.process(inputBuffer, tempBuffers[0].data(), numSamples);
+    mPreAmpStage->processBlock(tempBuffers[0].data(), tempBuffers[1].data(), numSamples);
 
-    // 6dB pad
-    for (int sample = 0; sample < numSamples; sample++) {
-      outputBuffer[sample] *= 0.5f;
+    // PreAmpStage -> AmpStage -> ClippingStage
+    mAmpStage.process(tempBuffers[1].data(), tempBuffers[2].data(), numSamples);
+    mClippingStage->processBlock(tempBuffers[2].data(), tempBuffers[3].data(), numSamples);
+
+    // Input -> FF2
+    mInputBuffer.process(inputBuffer, tempBuffers[4].data(), numSamples);
+    mFF2->processBlock(tempBuffers[4].data(), tempBuffers[5].data(), numSamples);
+
+    // FF1 from PreAmpStage
+    mFF1->processBlock(nullptr, tempBuffers[6].data(), numSamples); // FF1 doesn't use input
+
+    // Sum: FF1 + ClippingStage + FF2
+    for (int i = 0; i < numSamples; ++i) {
+      tempBuffers[7][i] = tempBuffers[6][i] + tempBuffers[3][i] + tempBuffers[5][i];
     }
 
-    mInputBufferProcessor.processBlock(outputBuffer, numSamples);
+    // Sum -> SummingAmp -> ToneControl -> OutputBuffer
+    mSummingAmp.process(tempBuffers[7].data(), tempBuffers[8].data(), numSamples);
+    mToneControl.process(tempBuffers[8].data(), tempBuffers[9].data(), numSamples);
+    mOutputBuffer.process(tempBuffers[9].data(), outputBuffer, numSamples);
+  }
 
-    // op amp clipping 
-    for (int sample = 0; sample < numSamples; sample++) {
-      float mSample = outputBuffer[sample];
-      outputBuffer[sample] > 4.5f ? 4.5f : mSample;
-      outputBuffer[sample] < -4.5f ? -4.5f : mSample;
-    }
+  // Parameter setters
+  void setGain(float gain) {
+    if (mPreAmpStage) mPreAmpStage->setGain(gain);
+    mAmpStage.setGain(gain);
+    if (mFF2) mFF2->setGain(gain);
+  }
 
-    // gain stage
-    gainStageProc->processBlock(outputBuffer);
+  void setTreble(float treble) {
+    mToneControl.setTreble(treble);
+  }
 
-    // tone stage 
-    // mToneFilterProcessor.setTreble(trebleParam);
-    mToneFilterProcessor.processBlock(outputBuffer, numSamples);
-
-    // inverting op amp with charge pump elevated supply
-    for (int sample = 0; sample < numSamples; sample++) {
-      outputBuffer[sample] *= -1.f;
-      float mSample = outputBuffer[sample];
-      outputBuffer[sample] > 11.7f ? 11.7f : mSample;
-      outputBuffer[sample] < -13.1f ? -13.1f : mSample;
-    }
-
-    // output stage
-    // mOutputStageProc.setLevel(levelParam);
-    mOutputStageProc.processBlock(outputBuffer, numSamples);
-
-    // remove dc
-    for (int sample = 0; sample < numSamples; sample++) {
-      outputBuffer[sample] = dcBlocker.processSample(outputBuffer[sample]);
-    }
-    
+  void setLevel(float level) {
+    mOutputBuffer.setLevel(level);
   }
   
 };
